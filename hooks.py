3cf7ab775f9f7ec44580a7acd0ef9147bccb41ff
jirotakeo <jirotakeo@gmail.com>
2021-07-17 00:02:51 +0300

    kjkjkds (#11)
    * kjkjkds
    
    * etere

--- hooks.py
+++ hooks.py
@@ -1,17 +1,1 @@
-print("пурум пум пум")
-'''
-testing
-'''
-
-print('hello')
-
-'''
-add new branch
-'''
-'''
-git commit main branch
-'''
-
-print('new pulls')
-
-print('sdadasdasd')
+print('еще один  pull')
